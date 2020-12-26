af0f8a2f2711957dbd2cc9ab651f8b7f5ac68062
ricardojunior02 <ricardojunior199502@gmail.com>
2020-12-26 15:50:52 -0300

    Model de Appointment

---
+++ src/models/Appointment.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const Appointment = new mongoose.Schema({
+  name: String,
+  email: String,
+  description: String,
+  cpf: String,
+  date: Date,
+  time: String,
+  finished: Boolean,
+  notified: Boolean
+});
+
+module.exports = mongoose.model('appointment', Appointment);
